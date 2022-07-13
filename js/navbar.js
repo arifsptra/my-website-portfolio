c237b646fce308fe4a8d398b9baec242f3191a10
arifsptra <arif.sptrra@gmail.com>
2022-07-13 21:09:51 +0700

    fix navbar

--- js/navbar.js
+++ js/navbar.js
@@ -13,3 +13,46 @@ bgSidebar.addEventListener("click", function () {
   sidebar.classList.remove("change");
   burger.classList.remove("change");
 });
+
+// for color navbar active
+const about = document.querySelector(".about");
+const education = document.querySelector(".education");
+const skills = document.querySelector(".skills");
+const portfolio = document.querySelector(".portfolio");
+const contact = document.querySelector(".contact");
+
+about.addEventListener("click", function () {
+  this.classList.toggle("active");
+  education.classList.remove("active");
+  skills.classList.remove("active");
+  portfolio.classList.remove("active");
+  contact.classList.remove("active");
+});
+education.addEventListener("click", function () {
+  this.classList.toggle("active");
+  about.classList.remove("active");
+  skills.classList.remove("active");
+  portfolio.classList.remove("active");
+  contact.classList.remove("active");
+});
+skills.addEventListener("click", function () {
+  this.classList.toggle("active");
+  about.classList.remove("active");
+  education.classList.remove("active");
+  portfolio.classList.remove("active");
+  contact.classList.remove("active");
+});
+portfolio.addEventListener("click", function () {
+  this.classList.toggle("active");
+  about.classList.remove("active");
+  education.classList.remove("active");
+  skills.classList.remove("active");
+  contact.classList.remove("active");
+});
+contact.addEventListener("click", function () {
+  this.classList.toggle("active");
+  about.classList.remove("active");
+  education.classList.remove("active");
+  skills.classList.remove("active");
+  portfolio.classList.remove("active");
+});
