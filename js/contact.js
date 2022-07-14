94f0bf644e0a4caec70b94aad33f874c8aa2f15e
arifsptra <arif.sptrra@gmail.com>
2022-07-14 21:29:41 +0700

    gives loading and alerts when sending messages

--- js/contact.js
+++ js/contact.js
@@ -1,9 +1,24 @@
 const scriptURL = "https://script.google.com/macros/s/AKfycbwLgvD299NMTYfBhDHkk12h2HU14IZz-TiGLOjtHHotVRmOu5qom3mP26-qKwHC8ey0Kw/exec";
 const form = document.forms["contact-form"];
+const btnSubmit = document.querySelector(".submit");
+const btnLoading = document.querySelector(".loading");
+const myAlert = document.querySelector(".myAlert");
+const closeMyAlert = document.querySelector(".close");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  btnLoading.classList.toggle("hide");
+  btnSubmit.classList.toggle("hide");
   fetch(scriptURL, { method: "POST", body: new FormData(form) })
-    .then((response) => console.log("Success!", response))
+    .then((response) => {
+      btnLoading.classList.toggle("hide");
+      btnSubmit.classList.toggle("hide");
+      myAlert.classList.toggle("hide");
+      form.reset();
+      console.log("Success!", response);
+    })
     .catch((error) => console.error("Error!", error.message));
+});
+closeMyAlert.addEventListener("click", function () {
+  myAlert.classList.toggle("hide");
 });
