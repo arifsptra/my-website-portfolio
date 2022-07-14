96435c84f351919a7d140550da7c12b62f85166f
arifsptra <arif.sptrra@gmail.com>
2022-07-14 10:41:27 +0700

    gives a smooth scrolling effect

--- js/navbar.js
+++ js/navbar.js
@@ -21,38 +21,138 @@ const skills = document.querySelector(".skills");
 const portfolio = document.querySelector(".portfolio");
 const contact = document.querySelector(".contact");
 
-about.addEventListener("click", function () {
+about.addEventListener("click", function (event) {
   this.classList.toggle("active");
   education.classList.remove("active");
   skills.classList.remove("active");
   portfolio.classList.remove("active");
   contact.classList.remove("active");
+  if (this.hash !== "") {
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      800,
+      function () {
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      }
+    );
+  }
 });
-education.addEventListener("click", function () {
+education.addEventListener("click", function (event) {
   this.classList.toggle("active");
   about.classList.remove("active");
   skills.classList.remove("active");
   portfolio.classList.remove("active");
   contact.classList.remove("active");
+  if (this.hash !== "") {
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      800,
+      function () {
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      }
+    );
+  }
 });
-skills.addEventListener("click", function () {
+skills.addEventListener("click", function (event) {
   this.classList.toggle("active");
   about.classList.remove("active");
   education.classList.remove("active");
   portfolio.classList.remove("active");
   contact.classList.remove("active");
+  if (this.hash !== "") {
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      800,
+      function () {
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      }
+    );
+  }
 });
-portfolio.addEventListener("click", function () {
+portfolio.addEventListener("click", function (event) {
   this.classList.toggle("active");
   about.classList.remove("active");
   education.classList.remove("active");
   skills.classList.remove("active");
   contact.classList.remove("active");
+  if (this.hash !== "") {
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      800,
+      function () {
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      }
+    );
+  }
 });
-contact.addEventListener("click", function () {
+contact.addEventListener("click", function (event) {
   this.classList.toggle("active");
   about.classList.remove("active");
   education.classList.remove("active");
   skills.classList.remove("active");
   portfolio.classList.remove("active");
+  if (this.hash !== "") {
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      800,
+      function () {
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      }
+    );
+  }
 });
